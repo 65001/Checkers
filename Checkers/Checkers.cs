43ecdc5b3b4c2830f322500ccab3dd992372dbf9
65001 <asathiabalan@gmail.com>
2017-05-25 18:28:32 -0400

    Basic Setup

---
+++ Checkers/Checkers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.SmallBasic.Library;
+using LitDev;
+
+namespace Checkers
+{
+    public class Game
+    {
+        public static void Main()
+        {
+            GraphicsWindow.Show();
+        }
+        public void Class1()
+        {
+        }
+    }
+
+    public class UI
+    {
+
+    }
+}
