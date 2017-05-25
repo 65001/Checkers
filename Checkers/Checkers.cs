1c21b35f247732fb6dc60f4563ce4cdc0a1a916d
65001 <asathiabalan@gmail.com>
2017-05-25 18:47:19 -0400

    DrawGrid

--- Checkers/Checkers.cs
+++ Checkers/Checkers.cs
@@ -9,14 +9,26 @@ namespace Checkers
         public static void Main()
         {
             GraphicsWindow.Show();
-        }
-        public void Class1()
-        {
+            LDGraphicsWindow.State = 2;
+            GraphicsWindow.Title = "Checkers";
+            UI.DrawBoard(100);
         }
     }
 
     public class UI
     {
-
+        public static void DrawBoard(int SizeofSquares,int Dimensions = 8)
+        {
+            int CellCount = 1;
+            for (int x = SizeofSquares; x <= SizeofSquares*Dimensions; x += SizeofSquares)
+            {
+                for (int y = SizeofSquares; y <= SizeofSquares * Dimensions; y += SizeofSquares)
+                {
+                    CellCount++;
+                    GraphicsWindow.DrawRectangle(x, y, SizeofSquares, SizeofSquares);
+                    Console.WriteLine("({0},{1}) : {2}", x/SizeofSquares, y/SizeofSquares, CellCount % 2);
+                }
+            }
+        }
     }
 }
