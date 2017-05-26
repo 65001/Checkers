9f27c9f35143688eccb8a15cb4a3339f86f8c6e5
65001 <asathiabalan@gmail.com>
2017-05-25 22:15:50 -0400

    GameBoard Done

--- Checkers/Checkers.cs
+++ Checkers/Checkers.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Microsoft.SmallBasic.Library;
 using LitDev;
 
@@ -6,12 +8,22 @@ namespace Checkers
 {
     public class Game
     {
+        
+        public readonly static int SizeofSquares = 100;
+        public readonly static int Dimensions = 8;
+        public static int[,] ColorBoard = new int[Dimensions,Dimensions];
+        public static string[,] ShapeBoard = new string[Dimensions,Dimensions];
+
+        public enum Color { Brown, Red, Empty };
+
         public static void Main()
         {
             GraphicsWindow.Show();
             LDGraphicsWindow.State = 2;
             GraphicsWindow.Title = "Checkers";
-            UI.DrawBoard(100);
+            UI.DrawBoard(SizeofSquares,Dimensions);
+
+            GraphicsWindow.MouseDown += Events.MD;
         }
     }
 
@@ -23,25 +35,64 @@ namespace Checkers
             {
                 for (int y = SizeofSquares; y <= SizeofSquares * Dimensions; y += SizeofSquares)
                 {
-                    int Modulo = (x/SizeofSquares+y/SizeofSquares) % 2;
+                    int xCord = x / SizeofSquares;
+                    int yCord = y / SizeofSquares;
+                    int Modulo =(int) (xCord + yCord) % 2;
                     GraphicsWindow.DrawRectangle(x, y, SizeofSquares, SizeofSquares);
 
                     if (Modulo == 1)
                     {
                         GraphicsWindow.BrushColor = LDColours.Black;
                         GraphicsWindow.FillRectangle(x, y, SizeofSquares, SizeofSquares);
-                        GraphicsWindow.DrawText(x + .25 * SizeofSquares, y + .5 * SizeofSquares, "(" + x / SizeofSquares + "," + y / SizeofSquares + ") : " + Modulo);
+                        GraphicsWindow.DrawText(x + .25 * SizeofSquares, y + .5 * SizeofSquares, "(" + xCord+ "," + yCord+ ") : " + Modulo);
                         GraphicsWindow.BrushColor = "White";
+                        if (y/SizeofSquares <= 3)
+                        {
+                            Game.ColorBoard[xCord - 1, yCord - 1] = (int) Game.Color.Red;
+
+                            Game.ShapeBoard[xCord -1,yCord -1] =
+                            GenerateCircle(x+SizeofSquares/6, y+SizeofSquares/6,(int)((decimal).65* SizeofSquares), "Red");
+                        }
+                        else if (y/SizeofSquares >= (Dimensions-2) )
+                        {
+                            Game.ColorBoard[xCord - 1, yCord -1] = (int)Game.Color.Brown;
+
+                            Game.ShapeBoard[xCord - 1, yCord - 1] =
+                            GenerateCircle(x + SizeofSquares / 6, y + SizeofSquares / 6, (int)((decimal).65 * SizeofSquares), "Brown");
+                        }
                     }
-                    else
-                    {
-                        GraphicsWindow.BrushColor = "Black";
-                    }
-                    string Message = "(" + x / SizeofSquares + "," + y / SizeofSquares + ")";
-                    GraphicsWindow.DrawText( (x + .5 * SizeofSquares) , y + .5 * SizeofSquares, Message);
                 }
             }
             GraphicsWindow.DrawRectangle(SizeofSquares, SizeofSquares, SizeofSquares * Dimensions, SizeofSquares * Dimensions);
+        }
+
+        public static string GenerateCircle(int x,int y,int radius,string Color)
+        {
+            GraphicsWindow.BrushColor = Color;
+            string _return = Shapes.AddEllipse(radius, radius);
+            Shapes.Move(_return, x, y);
+            return _return;
+        }
+
+    }
+
+    public class Events
+    {
+        public async static void MD()
+        {
+            Hanlders.Selected(GraphicsWindow.MouseX / Game.SizeofSquares, GraphicsWindow.MouseY / Game.SizeofSquares);
+            
+        }
+    }
+
+    public class Hanlders
+    {
+        public static void Selected(int xCord,int yCord)
+        {
+            if (xCord <= Game.Dimensions && yCord <= Game.Dimensions)
+            {
+                GraphicsWindow.ShowMessage("(" + xCord + "," + yCord + ") Control :" + Game.ShapeBoard[xCord - 1, yCord - 1] +" Color : " + Game.ColorBoard[xCord - 1, yCord - 1], null);
+            }
         }
     }
 }
